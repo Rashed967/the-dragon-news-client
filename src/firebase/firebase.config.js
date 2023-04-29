a4f02599d3a95c2546aa8e45590e9804f65b72ec
Rashed967 <rashedhasan967.com>
2023-04-29 19:38:11 +0600

    key loacl

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,13 +6,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 console.log("enviorment variable", import.meta.env.VITE_PASS)
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
-};
+    apiKey: "AIzaSyBZYYqGw7R6XrrO0bzJjTqCtRq6qmsI9wQ",
+    authDomain: "the-news-dragon-client-cf330.firebaseapp.com",
+    projectId: "the-news-dragon-client-cf330",
+    storageBucket: "the-news-dragon-client-cf330.appspot.com",
+    messagingSenderId: "211115461569",
+    appId: "1:211115461569:web:fd0637c56ad381f9e43d40"
+  };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
